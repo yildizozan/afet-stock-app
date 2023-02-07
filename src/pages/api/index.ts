528760d91255a80a9c1b3269be250d53fc98491f
Serhat SARI <serhhatsari@gmail.com>
2023-02-07 03:27:17 +0300

    added dummy endpoint

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,9 +1,20 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
+
+function getDatas() : Map<string,number >{
+    // Get data from another resource
+    let map = new Map<string, number>();
+    map.set("Isıtıcı", 100);
+    map.set("Mont", 200);
+    map.set("Yagmurluk", 300);
+    map.set("Bot", 200);
+    return map;
+}
+
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200)
     res.setHeader("Content-Type", "application/json")
-    res.end('{ "resp": "true" }')
+    res.json({"data" : Object.fromEntries(getDatas())});
 }
 
 export default handler;
