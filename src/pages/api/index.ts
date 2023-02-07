ee7471e4b03b7fd122f12be1bb3ee5ca2d2506a1
Serhat SARI <serhhatsari@gmail.com>
2023-02-07 06:29:44 +0300

    update firebase methods

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,13 +1,40 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import app from '../../configs/firestoreConfig'
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
+import { getFirestore, doc, setDoc, getDoc, getDocs, collection, addDoc } from 'firebase/firestore'
 import Item from '../../types/item'
 
 const db = getFirestore(app)
 
 async function addStock(req, res) {
   try {
-    await setDoc(doc(db, 'stocks', 'stocks'), {
+    // TODO: When added stock, only the certain field will be incremented
+    await setDoc(doc(db, "stocks", req.body.material_name), {
+      material_name: req.body.material_name,
+      stock_number: req.body.stock_number
+    })
+    res.status(201).json({ success: true })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
+}
+
+async function getStocks(req, res){
+  try {
+    const querySnapshot = await getDocs(collection(db, "stocks"));
+
+    // TODO: Create a list and append every item into that list
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+    res.status(201).json({ success: true })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
+}
+
+async function updateStock(req, res) {
+  try {
+    await setDoc(doc(db, 'stocks',req.body.material_name ), {
       material_name: req.body.material_name,
       stock_number: req.body.stock_number
     })
