a72ec88017687cf8942336789432b318adff7dc8
Ozan Yıldız <developer@yildizozan.com>
2023-02-07 05:51:05 +0300

    Update mock data

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,36 +1,36 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import app from '../../configs/firestoreConfig';
-import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import { NextApiRequest, NextApiResponse } from 'next'
+import app from '../../configs/firestoreConfig'
+import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
+import Item from '../../types/item'
 
-const db = getFirestore(app);
+const db = getFirestore(app)
 
 async function addStock(req, res) {
-    try {
-        await setDoc(doc(db, "stocks", "stocks"),{
-            material_name: req.body.material_name,
-            stock_number: req.body.stock_number
-        });
-        res.status(201).json({"success": true})
-    } catch (error) {
-        res.status(400).json({"success": false})
-    }
+  try {
+    await setDoc(doc(db, 'stocks', 'stocks'), {
+      material_name: req.body.material_name,
+      stock_number: req.body.stock_number
+    })
+    res.status(201).json({ success: true })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
-function getDatas(): Map<string, number> {
-  // Get data from another resource
-  let map = new Map<string, number>()
-  map.set('Isıtıcı', 100)
-  map.set('Mont', 200)
-  map.set('Yagmurluk', 300)
-  map.set('Bot', 200)
-  return map
+function getDatas(): Item[] {
+  return [
+    { type: 'Isitici', value: 100 },
+    { type: 'Mont', value: 200 },
+    { type: 'Yagmurluk', value: 300 },
+    { type: 'Bot', value: 400 }
+  ]
 }
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Content-Type', 'application/json')
 
   if (req.headers.dev == 'true') {
-    res.json({ data: Object.fromEntries(getDatas()) })
+    res.json({ data: getDatas() })
     return
   }
 
